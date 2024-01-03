34a14923cdca5c04e417a0169b975c89ac21184c
Josh-techie <youse2002@outlook.fr>
2024-01-04 00:49:12 +0100

    0. Keep every promise you make and only make promises you can keep

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,3 @@
 export default function getResponseFromAPI() {
-    return new Promise((resolve) => {
-      resolve({ status: 200, body: 'Success' });
-    });
-  }
+  return new Promise(() => {});
+}
