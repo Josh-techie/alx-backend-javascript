ae324067ccc155b61d7a393d0300e064dbe0652c
Josh-techie <youse2002@outlook.fr>
2024-01-04 00:31:50 +0100

    Bdina tani

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,8 +1,5 @@
-
 export default function getResponseFromAPI() {
     return new Promise((resolve) => {
-      // Assuming a successful API response with status 200 and body 'Success'
       resolve({ status: 200, body: 'Success' });
     });
   }
-  
