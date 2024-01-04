30926d69df7794dc49f4da76376c6694c2ad28e1
Josh-techie <youse2002@outlook.fr>
2024-01-04 12:05:40 +0100

    10-loops.eslint

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const ans = [];
-    for (const value of array) {
-      ans.push(appendString + value);
-    }
-  
-    return ans;
+  const ans = [];
+  for (const value of array) {
+    ans.push(appendString + value);
   }
+
+  return ans;
+}
