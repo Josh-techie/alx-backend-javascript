8f9b2162de9f58d56cbb8036efcd1f20466562dc
Josh-Techie <youse2002@outlook.fr>
2024-01-14 18:02:01 +0100

    3-get_ids_sum.js

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+export default function getStudentIdsSum(students) {
+  return students.reduce(
+    (accumulator, currentValue) => currentValue.id + accumulator,
+    0
+  );
+}
