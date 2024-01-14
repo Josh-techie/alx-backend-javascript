2684bd8a89ddbba11322b13c3c664a98ab425c9a
Josh-Techie <youse2002@outlook.fr>
2024-01-14 14:23:26 +0100

    0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,1 +1,19 @@
 // to add the code
+
+// Create a function named getListStudents that returns an array of objects.
+
+// Each object should have three attributes: id(Number), firstName(String), and location(String).
+
+// The array contains the following students in order:
+
+// Guillaume, id: 1, in San Francisco
+// James, id: 2, in Columbia
+// Serena, id: 5, in San Francisco
+
+function getListStudents() {
+    return [
+        { id: 1, firstname: 'Guillaume', location: 'San Fransisco' },
+        { id: 2, firstname: 'James', location: 'Columbia' },
+        { id: 5, firstname: 'Serena', location: 'San Fransisco' }
+    ]
+}
