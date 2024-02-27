de2881dd2cba0271b7e16c5939b87a4c7279237a
Josh-Techie <youse2002@outlook.fr>
2024-02-28 00:43:30 +0100

    🌟 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,22 @@
+// Write a welcome message to the standard output (console)
 process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
+// Listen for the 'readable' event on the standard input (console input)
+// This event is emitted when there's data available to read
 process.stdin.on("readable", () => {
-  const chunk = process.stdin.read();
+    // Read data from the standard input
+    const chunk = process.stdin.read();
 
-  if (chunk) {
-    process.stdout.write(`Your name is: ${chunk}`);
-  }
+    // If data was read (i.e., the user entered something), then...
+    if (chunk) {
+        // Write the user's input back to the standard output
+        process.stdout.write(`Your name is: ${chunk}`);
+    }
 });
 
+// Listen for the 'end' event on the standard input
+// This event is emitted when there's no more data to read (i.e., the user has pressed Ctrl+D)
 process.stdin.on("end", () => {
-  process.stdout.write("This important software is now closing\n");
+    // Write a closing message to the standard output
+    process.stdout.write("This important software is now closing\n");
 });
