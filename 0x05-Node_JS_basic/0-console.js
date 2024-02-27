4371a3264d97ab935959b727c22be3f0f2856f41
Josh-Techie <youse2002@outlook.fr>
2024-02-28 00:29:54 +0100

    🎈 0-console.js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,11 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Josh Techie <https://github.com/Josh-Techie>
+ * >
+ */
 function displayMessage(message) {
-    console.log(message);
+  console.log(message);
 }
 
 module.exports = displayMessage;
