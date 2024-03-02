4b5237873616ff2f6679a3da632e62fbfe19f383
Josh-Techie <youse2002@outlook.fr>
2024-03-02 02:19:35 +0100

    5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,27 +1,29 @@
-// Create a more complex HTTP server using Node's HTTP module
 const http = require("http");
 const countStudents = require("./3-read_file_async");
 
+const hostname = "127.0.0.1";
 const port = 1245;
 
-const app = http.createServer((req, res) => {
+const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
   if (req.url === "/") {
-    res.write("Hello Holberton School!");
-  }
-  if (req.url === "/students") {
-    res.write("This is the list of our students\n");
-    countStudents(process.argv[2])
-      .then((data) => {
-        res.end(data.join("\n"));
+    res.end("Hello Holberton School!");
+  } else if (req.url === "/students") {
+    let dbInfo = "This is the list of our students\n";
+    await countStudents(process.argv[2])
+      .then((msg) => {
+        dbInfo += msg;
+        res.end(dbInfo);
       })
       .catch((err) => {
-        res.end(err.message);
+        dbInfo += err.message;
+        res.end(dbInfo);
       });
   }
 });
 
-app.listen(port);
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
+});
 
 module.exports = app;
